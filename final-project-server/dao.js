c87c43e1a0d0f126f63f2359b40ddf0eaa2c5c09
Sufiyan Sadiq Saboowala <ssaboowa@isimtl.local>
2020-04-06 00:56:25 -0400

    Upadted server with new methods

--- final-project-server/dao.js
+++ final-project-server/dao.js
@@ -1,20 +1,51 @@
-const { Pool } = require('pg');
+const pg = require('pg');
+pg.defaults.poolSize = 100;
 
-const pool = new Pool({
-    "host" : "localhost",
-    "port" : 5432,
-    "user" : "postgres",
-    "password" : "admin"
+const pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    database: 'final-project-music-albums',
+    password: 'admin'
 });
 
 const getPlaylists = async (request, response) => {
     try {
-        await pool.connect();
+        const client = await pool.connect();
         const results = await pool.query('SELECT * from playlist');
-        response.status(200).send(results.rows);
+        response.status(200).json(results.rows);
+        client.release();
     } catch (e) {
-        response.status(400).send(e);
+        response.status(400).send(e);g
     }
 };
 
-module.exports.getPlaylists = getPlaylists;
+const postTrack = async (req, res) => {
+    try {
+        const { id, playlist_id: playlistId, title, uri, master_id: masterId } = req.body;
+        const client = await pool.connect();
+        const results = await pool.query(`INSERT INTO track VALUES (${id}, ${playlistId}, '${title}', '${uri}',${masterId});`);
+        res.status(200).json({ "Response": 'Insert Successful', "Affected Rows": results.rowCount });
+        client.release();
+    } catch (e) {
+        res.status(400).json({ "Error": e.detail, "constraint": e.constraint, "table": e.table });
+    }
+};
+
+const getPlaylist = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const client = await pool.connect();
+        const results = await pool.query(`SELECT * from track WHERE playlist_id = ${id}`);
+        res.status(200).json(results.rows);
+        client.release();
+    } catch (e) {
+        res.status(400).json({ "Error": e.detail, "constraint": e.constraint, "table": e.table });
+    }
+}
+
+module.exports = {
+    getPlaylists,
+    postTrack,
+    getPlaylist
+};
--- final-project-server/server.js
+++ final-project-server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const pgDao = require('./dao');
+const cors = require('cors')
 const app = express();
+
 const PORT = 3500;
 
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.get('/', pgDao.getPlaylists);
+app.get('/api/playlist', pgDao.getPlaylists);
+app.get('/api/playlist/:id', pgDao.getPlaylist);
+app.post('/api/playlist', pgDao.postTrack);
